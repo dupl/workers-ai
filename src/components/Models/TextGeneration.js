f6213239367f01440a37da2253000ea3ea34c553
dupl <67990457+dupl@users.noreply.github.com>
2025-01-03 00:22:35 +0800

    更新 TextGeneration.js

--- src/components/Models/TextGeneration.js
+++ src/components/Models/TextGeneration.js
@@ -6,7 +6,7 @@ import { ArrowUpOutlined } from "@ant-design/icons";
 const TextGeneration = () => {
     const [value, setValue] = useState('');
     const [generatedText, setGeneratedText] = useState([]);
-    const model = '@cf/meta/llama-3.1-70b-instruct';
+    const model = '@cf/meta/llama-3.3-70b-instruct-fp8-fast';
     const messagesEndRef = useRef(null);
     const [loading, setLoading] = useState(false); // 新增状态变量
 
